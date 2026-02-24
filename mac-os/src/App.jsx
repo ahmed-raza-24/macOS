8b6b3790ec390dcb202950468bd11986f2839571
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-02-24 20:56:46 +0530

    npm i -S rend

--- mac-os/src/App.jsx
+++ mac-os/src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import "./app.scss"
 import Dock from './components/Dock'
 import Nav from './components/Nav'
-import Github from './components/windows/Github'
-import Note from './components/windows/Note'
-import Resume from './components/windows/Resume'
-import Spotify from './components/windows/Spotify'
-import Cli from './components/windows/Cli'
+// import Github from './components/windows/Github'
+// import Note from './components/windows/Note'
+// import Resume from './components/windows/Resume'
+// import Spotify from './components/windows/Spotify'
+// import Cli from './components/windows/Cli'
 
 function App() {
 
---
+++ mac-os/src/components/windows/MacWindows.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Rnd} from 'react-rnd'
+
+const MacWindows = () => {
+  return (
+    <Rnd>
+        <img src=''/>
+    </Rnd>
+  )
+}
+
+export default MacWindows
