e8b7eeaedfa9f3c003f26f5f954b9d3266587ce7
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-02-24 21:11:50 +0530

    MacWindows.jsx updated

--- mac-os/src/components/windows/MacWindows.jsx
+++ mac-os/src/components/windows/MacWindows.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import {Rnd} from 'react-rnd'
+import { Rnd } from 'react-rnd'
+import "./window.scss"
 
-const MacWindows = () => {
-  return (
-    <Rnd>
-        <img src=''/>
-    </Rnd>
-  )
+const MacWindow = ({ children, width = "40vw", height = "40vh", windowName, setWindowsState }) => {
+    return (
+        <Rnd
+            default={{
+                width: width,
+                height: height,
+                x: 300,
+                y: 200
+            }}
+        >
+            <div className="window">
+                <div className="nav">
+                    <div className="dots">
+                        <div onClick={() => setWindowsState(state => ({ ...state, [windowName]: false }))} className="dot red"></div>
+                        <div className="dot yellow"></div>
+                        <div className="dot green"></div>
+                    </div>
+
+                    <div className="title"><p>ahmedraza</p></div>
+
+                </div>
+                <div className="main-content">
+                    {children}
+                </div>
+            </div>
+        </Rnd>
+    )
 }
 
-export default MacWindows
+export default MacWindow
